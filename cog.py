960902fee2d670f75eb0d6d98de8f4b665e95fe5
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-02-03 19:11:17 +0530

    Update cog.py

--- cog.py
+++ cog.py
@@ -1,1 +1,9 @@
-print("cog loaded")
+    @commands.slash_command(name="ping", description="Get a pong message with response time.")
+    async def ping(self, ctx):
+        start_time = time.time()
+        embed = discord.Embed(description="Pong!", color=0x2f3136)
+        response = await ctx.respond(embed=embed)
+        end_time = time.time()
+        duration = (end_time - start_time) * 1000
+        embed.description = f"Pong! {duration:.2f}ms."
+        await response.edit(embed=embed)
