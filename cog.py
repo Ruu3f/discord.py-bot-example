cad5fa9b297575d9c910e4b6b457cca8897e23c9
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-02-03 19:33:47 +0530

    Update cog.py

--- cog.py
+++ cog.py
@@ -1,20 +1,16 @@
+#note: this works but this is just a simple example of response
 import discord
 from discord.ext import commands
-from psutil import Process
 
 class cog(commands.Cog): #this is the class
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(name="ping", description="Get a pong message with response time.") #name="ping" is the name of the command, description"Get a pong message with response time." is the description/help of a command
+    @commands.command(name="ping", description="get a response.") #name="ping" is the name of the command, description"get a response" is the description/help of a command
     async def ping(self, ctx):
         start_time = time.time()
         embed = discord.Embed(description="Pong!", color=0x69696) #you can change the color using hex codes
-        response = await ctx.respond(embed=embed)
-        end_time = time.time()
-        duration = (end_time - start_time) * 1000
-        embed.description = f"Pong! {duration:.2f}ms."
-        await response.edit(embed=embed)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(cog(bot)) #choose any name you want just keep it same as the class name
