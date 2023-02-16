cec6b3ce0ce1ee75686a7443b8520ccfbb21c65c
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-02-17 00:49:57 +0530

    Update index.py

--- legacy-bot/index.py
+++ legacy-bot/index.py
@@ -3,14 +3,13 @@ import os
 os.system("pip install -r requirements.txt") #install packages, a requirements file is required
 import discord
 from discord.ext import commands
-from colorama import Fore #colors for text (opt)
 
 intents = discord.Intents.all() #you can change intents as per your usage, on default i've enabled every intent
 bot = commands.Bot(command_prefix = "!", intents = intents) #you can change prefix to anything here if you dont want the ! prefix
 
 @bot.event
 async def on_ready():
-	print(f"{Fore.GREEN} {bot.user.name} is ready!") #the message it shows when the bot is ready
+	print(f"{bot.user.name} is ready!") #the message it shows when the bot is ready
 	await bot.change_presence(status = discord.Status.online, #set the bots status, can be dnd, online or idle.
               activity = discord.Activity(type = discord.ActivityType.watching, #you can set watching to something else like playing, listening, etc.
               name = "change this in index.py")) #change the status message of the bot
